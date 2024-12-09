67f451b954194eed93ecc42dc622f04609d4cf7b
amogha r <amoghar23@gmail.com>
2024-12-09 20:46:03 +0530

    updated suggestion route to /suggestion from /suggestions

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -8,11 +8,9 @@ import PostEvent from '../pages/admin/PostEvent'
 import Feedback from '../pages/users/Feedback';
 import Winners from '../pages/users/Winners'
 import Suggestions from '../pages/users/Suggestions'
-// import EventsListing from '../pages/test';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer'
-import { SuccessCard } from '../components/common/SuccessCard';
-import { FailureCard } from '../components/common/FailureCard';
+
 export default function Router() {
   return (
     <div className='App'>
@@ -28,9 +26,8 @@ export default function Router() {
           <Route path="/feedback" element={<Feedback />} />
           <Route path="/admin/post-event" element={<PostEvent />} />
           <Route path="/winners" element={<Winners/>}/>
-          <Route path="/suggestions" element={<Suggestions/>}/>
-          <Route path="/test" element = {<SuccessCard/>}/>
-          <Route path="/test1" element = {<FailureCard/>}/>
+          <Route path="/suggestion" element={<Suggestions/>}/>
+
         </Routes>
       </div>
       <Footer/>
